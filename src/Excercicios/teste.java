0e6db0cc734ef9129436d2e99fe6a8956623cdb3
Wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2017-08-03 16:20:09 -0300

    Teste

---
+++ src/Excercicios/teste.java
@@ -0,0 +1,8 @@
+package Excercicios;
+import java.util.Scanner;
+public class teste {
+	public static void main(String args[]){
+		Scanner entrada=new Scanner(System.in);
+		System.out.print("oioiio");
+	}
+}
